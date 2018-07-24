35ca028296645a791128c4be26ed81b0cb047f9a
7990satyam200 <7990satyam200@gmail.com>
2018-07-24 12:41:13 +0530

    local

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flickclone.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LocalLibrary.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
